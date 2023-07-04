726de4324999d56a0fd724203106551050b65014
buryk20 <73736907+buryk20@users.noreply.github.com>
2023-07-04 18:02:44 +0300

    svep

--- js/slider.js
+++ js/slider.js
@@ -12,6 +12,39 @@ export let SliderBr = class {
     sliderMove() {
         this.getClickNext();
         this.getClickPrev();
+        this.moveImg();
+    }
+
+    moveImg() {
+        this.isClicked = false;
+        this.positionX;
+        this.wrpSlider.addEventListener('mousedown', (event) => {
+            this.isClicked = true;
+            this.positionX = event.clientX;
+            console.log(this.isClicked);
+
+        });
+        console.log(this.isClicked);
+
+        this.wrpSlider.addEventListener('mousemove', (event) =>  {
+            if(this.isClicked) {
+                if(this.counter === 0 || this.counter === 1) {
+                    this.counter = 1;
+                    console.log(this.counter);
+                    window.getComputedStyle(this.wrpSlider).getPropertyValue('--counter-import-slider');
+                    this.wrpSlider.style.setProperty('--counter-import-slider', -((this.positionX - event.clientX)) + 'px');
+                }
+            }
+        });
+
+        window.addEventListener('mouseup', (event) => {
+
+             this.isClicked = false;
+             if(this.positionX - event.clientX) {
+                 this.counter = this.counter + 1;
+                 this.actualTransition(this.counter);
+             }
+        })
     }
 
     getHtmlElem(nameElem) {
